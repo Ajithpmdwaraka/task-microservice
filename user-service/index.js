a4abe400f83f0e0650b0c6fb113a277ad168e42c
Ajith PM <ajithpmdwaraka@gmail.com>
2025-10-02 19:24:06 +0530

    chor: db connected

--- user-service/index.js
+++ user-service/index.js
@@ -1,7 +1,28 @@
 
 const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+
 const app = express()
 const port = 3001
+
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost:27017/notes')
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('Error connecting to MongoDB:', error)
+    })
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+})
+
+const User = mongoose.model('User', userSchema);
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
